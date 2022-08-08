c8b2f79a24493b499ca76cb3ac49af152cf1164b
sspacenerd <63374594+sspacenerd@users.noreply.github.com>
2022-08-08 10:50:29 -0400

    Revert "Machine"
    This reverts commit 1a571fa98ae8e836112d983d5e1048d53248ed6d.

--- Proyecto diplomado/Assets/Scripts/PickUp.cs
+++ Proyecto diplomado/Assets/Scripts/PickUp.cs
@@ -43,10 +43,6 @@ public class PickUp : MonoBehaviour
                     isPicked = false;
                     pickedGameObejct = null;
                 }
-                if(hit.transform.tag == "Safe" && hit.transform.gameObject.GetComponent<Machine>().canTouch)
-                {
-                    StartCoroutine(hit.transform.GetComponent<Machine>().Rotation());
-                }
             }
         }
         if (Input.GetKeyDown(KeyCode.E)) //Si yo presiono la E
